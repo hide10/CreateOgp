f3b0398d5ea67b9a80a98dd8c05f4cee46c742f1
小林 秀人 <kobayashi@msint.com>
2022-03-24 13:55:15 +0900

    文字列折り返しにtextwrapライブラリを使うように修正

--- ogp.py
+++ ogp.py
@@ -2,19 +2,19 @@
 # https://ktaskn.jp/post/9
 
 import os
+import textwrap
 from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
 
-STRIP_WIDTH = 1200
-STRIP_HEIGHT = 630
+OGP_WIDTH = 1200
+OGP_HEIGHT = 630
 FILE_FONT = "C:\\Users\\kobayashi\\Dropbox\\blog\\OgpMake\\Noto_Sans_JP\\NotoSansJP-Light.otf"
 
 def make_ogp(text):
-    # テキストを必要に応じて折り返す
-    text = insert_return(text)
+    text = textwrap.fill(text,10)
 
     # イメージデータを初期化
-    im = Image.new("RGB", (STRIP_WIDTH, STRIP_HEIGHT), "#309AC1")
+    im = Image.new("RGB", (OGP_WIDTH, OGP_HEIGHT), "#309AC1")
     draw = ImageDraw.Draw(im)
 
     # フォントを読み込む
@@ -22,7 +22,7 @@ def make_ogp(text):
     # フォントの高さを計算する
     text_width, text_height = draw.textsize(text, font)
     # フォントの出力位置（画像の概ね真ん中）を計算する
-    position = ((STRIP_WIDTH - text_width) / 2, (STRIP_HEIGHT - text_height) / 2 - 50)
+    position = ((OGP_WIDTH - text_width) / 2, (OGP_HEIGHT - text_height) / 2 - 50)
     # 元画像にテキストを合成
     draw.text(position, text, font=font)
 
@@ -30,15 +30,5 @@ def make_ogp(text):
     # buffer = BytesIO()
     im.save("test.png", format="PNG")
     im.show()
-
-# 文字を折り返すメソッド
-# もっといい方法考えるべきだが、今回はざっくりと10文字ごとに折り返す
-def insert_return(text):
-    if len(text) > 20:
-        return text[:10] + "\n" + text[10:20] + "\n" + text[20:]
-    elif len(text) > 10:
-        return text[:10] + "\n" + text[10:20]
-    else:
-        return text
 
 make_ogp("新型コロナワクチン接種、3回目は人生最悪クラスの悪寒を経験する")
