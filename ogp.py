eca27e7a1c58f2e257a3e825a010cfb22c567ebe
小林 秀人 <kobayashi@msint.com>
2022-03-24 14:12:46 +0900

    出力ファイル名を入力テキストベースに変更 ソースコード整理

--- ogp.py
+++ ogp.py
@@ -1,32 +1,28 @@
-import os
 import textwrap
+from xml.dom.expatbuilder import TEXT_NODE
 from PIL import Image, ImageDraw, ImageFont
-from io import BytesIO
 
 OGP_WIDTH = 1200
 OGP_HEIGHT = 630
-FILE_FONT = "C:\\Users\\kobayashi\\Dropbox\\blog\\OgpMake\\Noto_Sans_JP\\NotoSansJP-Light.otf"
+TEXT_WRAP = 10
+BG_COLOR = "#309AC1"
+FONT_FILE = "C:\\Users\\kobayashi\\Dropbox\\blog\\OgpMake\\Noto_Sans_JP\\NotoSansJP-Light.otf"
 
 
-def make_ogp(text):
-    text = textwrap.fill(text, 10)
-
-    im = Image.new("RGB", (OGP_WIDTH, OGP_HEIGHT), "#309AC1")
+def create_ogp(text):
+    im = Image.new("RGB", (OGP_WIDTH, OGP_HEIGHT), BG_COLOR)
     draw = ImageDraw.Draw(im)
 
-    # フォントを読み込む
-    font = ImageFont.truetype(FILE_FONT, 90)
-    # フォントの高さを計算する
-    text_width, text_height = draw.textsize(text, font)
-    # フォントの出力位置（画像の概ね真ん中）を計算する
+    font = ImageFont.truetype(FONT_FILE, 90)
+    # テキストを折り返す
+    wrap_text = textwrap.fill(text, TEXT_WRAP)
+    text_width, text_height = draw.textsize(wrap_text, font)
     position = ((OGP_WIDTH - text_width) / 2,
                 (OGP_HEIGHT - text_height) / 2 - 50)
-    # 元画像にテキストを合成
-    draw.text(position, text, font=font)
+    draw.text(position, wrap_text, font=font)
 
-    # PNGに変換
-    im.save("test.png", format="PNG")
+    im.save(text + ".png", format="PNG")
     im.show()
 
 
-make_ogp("新型コロナワクチン接種、3回目は人生最悪クラスの悪寒を経験する")
+create_ogp("新型コロナワクチン接種、3回目は人生最悪クラスの悪寒を経験する")
